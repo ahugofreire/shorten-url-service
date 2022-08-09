bc7d5d0390dc75cca07349b1d662859b1eb0ada9
Hugo Freire <hugo@LAPTOP-CBU818UF.localdomain>
2022-08-08 21:12:43 -0300

    fix file

--- api/database/database.go
+++ api/database/database.go
@@ -10,6 +10,7 @@ import (
 var Ctx = context.Background()
 
 func CreateClient(dbNo int) *redis.Client {
+	
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     os.Getenv("DB_ADDR"),
 		Password: os.Getenv("DB_PASS"),
